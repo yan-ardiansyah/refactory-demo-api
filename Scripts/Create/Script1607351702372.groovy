f9ef6b6f70ec6c7844d7e082dc805dfa1f798df2
yan-ardiansyah <yan.ardiansyah90@gmail.com>
2020-12-08 11:28:23 +0700

    Add function to check integer value type

--- Scripts/Create/Script1607351702372.groovy
+++ Scripts/Create/Script1607351702372.groovy
@@ -15,16 +15,32 @@ import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
 import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
 import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
 import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
 
+'Send Request to POST https://reqres.in/api/users'
 response = WS.sendRequest(findTestObject('Create'))
 
+'Call JSON Slurper function'
 def slurper = new groovy.json.JsonSlurper()
 
+'Parse the response and save in variable "result"'
 def result = slurper.parseText(response.getResponseBodyContent())
 
+'Take the "id" value and save in "ids" variable'
 ids = result.id
 
+'print the "ids" variable'
 println(ids)
 
-WS.getElementText(response, 'id')
+'Extract value of "ids" and get last of the char and save it to "lastid" char'
+lastid = ids.substring(2)
+
+'print last char and save it to "lastid" variable'
+println(lastid)
+
+'Change the value type of "lastid" variable to Integer and save it to "id_int" variable'
+id_int = Integer.parseInt(lastid)
+
+'Compare the "lastid" variable value and "id_int" variable value'
+WS.verifyEqual(lastid, id_int, FailureHandling.STOP_ON_FAILURE)
 
